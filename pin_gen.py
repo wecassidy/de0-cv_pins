4f064c29f2b66e89c874b6854da7fa0c46b7a877
wecassidy <wec.notif.github@gmail.com>
2021-03-06 18:52:56 -0500

    Create command loop with list command

--- pin_gen.py
+++ pin_gen.py
@@ -4,6 +4,7 @@
 Generate a Quartus settings file to map FPGA pins for the Altera DE0-CV.
 """
 
+import cmd
 import csv
 
 
@@ -17,8 +18,9 @@ def list_assignments(mapping):
 
 
 # Load pin dicts
+groups = ("clock", "hex", "keys", "led", "switches")
 pins = dict()
-for group in ("clock", "hex", "keys", "led", "switches"):
+for group in groups:
     pins[group] = dict()
     with open(f"{group}.csv", newline="") as f:
         reader = csv.reader(f)
@@ -28,3 +30,17 @@ for group in ("clock", "hex", "keys", "led", "switches"):
             ):  # Skip blank lines and comments
                 continue
             pins[group][row[0]] = row[1]
+
+# Assignment command loop
+class AssignLoop(cmd.Cmd):
+    intro = "Pin mapper, but fast! Type help or ? to list commands.\n"
+    prompt = "> "
+    mapping = {group: dict() for group in groups}
+
+    def do_list(self, _):
+        """Print current assignments."""
+        list_assignments(self.mapping)
+
+
+if __name__ == "__main__":
+    AssignLoop().cmdloop()
