a777c6756072ae5b1cc3a1efa512055e57d27df5
wecassidy <wec.notif.github@gmail.com>
2021-03-06 18:43:51 -0500

    Write function to print pin assignments

--- pin_gen.py
+++ pin_gen.py
@@ -6,6 +6,16 @@ Generate a Quartus settings file to map FPGA pins for the Altera DE0-CV.
 
 import csv
 
+
+def list_assignments(mapping):
+    for name, group in mapping.items():
+        if any(group):
+            print(name)
+            for node, pin in group.items():
+                print(f"{node:>15}  →  {pin}")
+            print()
+
+
 # Load pin dicts
 pins = dict()
 for group in ("clock", "hex", "keys", "led", "switches"):
